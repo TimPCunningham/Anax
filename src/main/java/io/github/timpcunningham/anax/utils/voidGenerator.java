1aff9578bf2a68ffcfb48341d0442297ca7ca7a9
PepsiDog <pepsidawg00@gmail.com>
2016-05-19 13:26:59 -0400

    Add void generator

---
+++ src/main/java/io/github/timpcunningham/anax/utils/voidGenerator.java
@@ -0,0 +1,13 @@
+package io.github.timpcunningham.anax.utils;
+
+import org.bukkit.World;
+import org.bukkit.generator.ChunkGenerator;
+
+import java.util.Random;
+
+public class voidGenerator extends ChunkGenerator {
+    @Override
+    public byte[] generate(World world, Random random, int x, int z) {
+        return new byte[32768];
+    }
+}

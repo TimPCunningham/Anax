ecd145478dbc8e35c053d2957898616637bf13aa
PepsiDog <pepsidawg00@gmail.com>
2016-05-31 14:05:48 -0400

    Update message format

--- src/main/java/io/github/timpcunningham/anax/player/Channel.java
+++ src/main/java/io/github/timpcunningham/anax/player/Channel.java
@@ -4,7 +4,7 @@ import java.text.MessageFormat;
 
 public enum Channel {
     ADMIN("`7[`6A`7] `b{0}`7: `f{1}","anax.chat.admin"),
-    GLOBAL("`7<`b{0}`7>: `f{1}", "anax.chat.all"),
+    GLOBAL("`7<`b{0}`7>: `f{1}", "anax.chat.global"),
     WORLD("`7[`bWorld`7] `b{0}`7: `f{1}", "anax.chat.world");
 
     private String format;
